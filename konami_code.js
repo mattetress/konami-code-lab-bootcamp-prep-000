0674a3c8323d7bfaf0b934543412d5d308215e7d
Matthew Etress <mattetress@me.com>
2018-09-04 21:23:38 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,7 +15,7 @@ const codes = [
 function init() {
   let index = 0;
 
-  document.body.addEventListener('keydown', onKeyDownHandler(e));
+  document.body.addEventListener('keydown', onKeyDownHandler);
 
   function onKeyDownHandler(e) {
     const key = e.key;
