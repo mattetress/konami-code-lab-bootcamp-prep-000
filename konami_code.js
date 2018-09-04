c854e0eff51ca2063d66a60017cbc138840d16f8
Matthew Etress <mattetress@me.com>
2018-09-04 21:20:40 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -22,7 +22,7 @@ function init() {
     if (key === codes[index]) {
       index++;
 
-      if (index === codes.length) {
+      if (index === codes.length - 1) {
         alert("You are the code master!");
 
         index = 0;
